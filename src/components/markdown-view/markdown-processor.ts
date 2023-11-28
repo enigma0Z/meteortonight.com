7a5553b524faf5a37de401991ac6864dfe8ebfd1
Johnathan Bell <enigma.0Z@gmail.com>
2023-11-27 23:45:49 -0500

    Change processors to eliminate pokemon processors, add link bubble

--- src/components/markdown-view/markdown-processor.ts
+++ src/components/markdown-view/markdown-processor.ts
@@ -1,34 +1,43 @@
-import { Find, Replace, findAndReplace } from 'mdast-util-find-and-replace';
-import PokeAPI from 'pokedex-promise-v2';
+import { Find, Replace, findAndReplace, ReplaceFunction } from 'mdast-util-find-and-replace';
 import { Plugin } from 'unified'
+import { PhrasingContent } from 'mdast'
 
 const RE_BUBBLE = /{bubble:.+?}/g;
+const RE_META = /{meta:.+?}/g;
 
 // Creating HTML node in Markdown node is undocumented.
 // https://github.com/syntax-tree/mdast-util-math/blob/e70bb824dc70f5423324b31b0b68581cf6698fe8/index.js#L44-L55
 
 export default function plugin(): Plugin {
-  function linkBubble(text: string): any {
-    return {
-      type: 'text',
-      meta: null,
-      value: text,
+  function linkBubble(text: string): PhrasingContent[] {
+    console.log('linkBubble', text)
+    const [icon, url, value]: string[] = text.split(':')
+
+    return [{
+      type: 'html',
+      value: value,
       data: {
-        hName: 'span',
+        hName: 'a',
         hProperties: {
-          className: ['link-bubble'],
+          className: ['link-bubble', `link-bubble-${icon}`],
+          href: `https://${url}`,
         },
-        hChildren: [{ type: 'text', value: text }],
+        hChildren: [
+          {
+            type: 'text', value: value
+          }
+        ],
       },
-    };
+    }];
   }
 
   function replaceBubble(match: string) {
-    return linkBubble(match.replace(/{type:(\w+?)}/, '$1'))
+    return linkBubble(match.replace(/{bubble:(.+?)}/, '$1'))
   }
 
   const replacers: [Find, Replace][] = [
     [RE_BUBBLE, replaceBubble],
+    [RE_META, '']
   ];
 
   function transformer(tree: any) {
