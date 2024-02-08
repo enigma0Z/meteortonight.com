5575642b8df25a325b7e8e54d9a741ad6ea5e1e2
Johnathan Bell <enigma.0Z@gmail.com>
2024-02-07 20:44:49 -0500

    Add YT embed feature

--- public/md/tone-tutorials/guitar/come-as-you-are.md
+++ public/md/tone-tutorials/guitar/come-as-you-are.md
@@ -2,22 +2,23 @@
 
 ![Come As You Are FX Stack](/img/tone-tutorials/guitar/come-as-you-are.png#left)
 
-Signal flow goes top down, right-click or long press and view image to see a
-detailed version.
+Signal flow goes top down.  Tap or click on the image for a bigger view.
 
 The guitar is an Epiphone Les Paul Trad Pro from like fifteen years ago, and
 this is from the bridge pickup.  I think single coils from like a Stratocaster
-may sound a tad better but this is what I have.  It still sounds pretty close
-IMO.
+or Mustang (I'm not sure what is on the original album) may sound a tad better
+but this is what I have.  It still sounds pretty close to me IMO.
 
 I used the "Almost Clean Matchlock" and "Clean A" presets from Kuassa's
 incredible amp sims.  You can get these plugins for Reason Studio (My DAW of
-choice) or as a VST, or in Kuassa's Amplifikation 360.  (Not sponsored, btw,
-just really love Kuassa's sims).
+choice) or as a VST, or in Kuassa's Amplifikation 360.  Not sponsored, btw,
+just really love Kuassa's amp sims.
 
 This is run into a BBD (Bucket Brigade Delay) style chorus, which is similar to
 the Small Clone that Cobain used on this song.  The settings are _almost_
 default, but I turned down the mod rate just a tad and tweaked the delay until
 the resonance felt right with my guitar, YMMV.
 
-Example: https://www.youtube.com/v/9ZRiW9669Y0
+## Tone Example
+
+{yt-embed::vertical::9ZRiW9669Y0}
--- src/components/markdown-view/markdown-processor.ts
+++ src/components/markdown-view/markdown-processor.ts
@@ -3,6 +3,7 @@ import { Plugin } from 'unified'
 import { PhrasingContent } from 'mdast'
 
 const RE_BUBBLE = '{bubble::(.+?)}'
+const RE_YT_EMBED = '{yt-embed::(.+?)}'
 const RE_CATCHALL = '{.+?}'
 
 // Creating HTML node in Markdown node is undocumented.
@@ -48,8 +49,42 @@ export default function plugin(): Plugin {
     return linkBigBubble(match.replace(RegExp(RE_BUBBLE, 'g'), '$1'))
   }
 
+  function ytEmbed(text: string): PhrasingContent[] {
+    const [orientation, videoId]: string[] = text.split('::')
+    /**
+     * <iframe 
+     *   width="806" 
+     *   height="778" 
+     *   src="https://www.youtube.com/embed/9ZRiW9669Y0" 
+     *   title="I know you’ve done this. We all have. #guitar #nirvana #comeasyouare" 
+     *   frameborder="0" 
+     *   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+     *   allowfullscreen></iframe>
+     */
+    return [{
+      type: 'html',
+      value: '',
+      data: {
+        hName: 'iframe',
+        hProperties: {
+          // style: {display: 'block', marginLeft: 'auto', marginRight: 'auto'},
+          src: `https://www.youtube.com/embed/${videoId}`,
+          allow: 'autoplay; picture-in-picture; web-share',
+          width: orientation === 'vertical' ? 450 : 800,
+          height: orientation === 'vertical' ? 800 : 450,
+          frameborder: 0,
+        }
+      }
+    }]
+  }
+
+  function replaceYtEmbed(match: string) {
+    return ytEmbed(match.replace(RegExp(RE_YT_EMBED, 'g'), '$1'))
+  }
+
   const replacers: [Find, Replace][] = [
     [RegExp(RE_BUBBLE, 'g'), replaceBigBubble],
+    [RegExp(RE_YT_EMBED, 'g'), replaceYtEmbed],
     [RegExp(RE_CATCHALL, 'g'), '']
   ];
 
